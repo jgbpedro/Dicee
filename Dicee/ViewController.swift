662a14dda8c8dae6392bf1fd6b45384fc1f58787
jgbpedro <jguilhermebp@live.com>
2020-10-22 20:35:34 -0300

    Add files via upload

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by José Guilherme on 16/10/20.
+//  Copyright © 2020 José Guilherme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateDiceImages()
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages(){
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6)) + 1
+        randomDiceIndex2 = Int(arc4random_uniform(6)) + 1
+        
+        diceImageView1.image = UIImage(named: "dice\(randomDiceIndex1)")
+        diceImageView2.image = UIImage(named: "dice\(randomDiceIndex2)")
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        updateDiceImages()
+        
+    }
+    
+    
+}
+
